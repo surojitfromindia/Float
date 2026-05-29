592fce8f9d662d1a24b9ebef4eb8852678635617
Surojit Paul <surojit99923@gmail.com>
2026-05-29 22:55:23 +0530

    Initial Commit

---
+++ Float/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Float
+//
+//  Created by SUROJIT PAUL on 29/05/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
